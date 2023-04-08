292cfe866dbc104c179ac8d3db7825f53cc59033
derek <me@derekwilliams.biz>
2023-04-08 17:23:45 -0400

    Fixed test spec config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
-    specPattern: 'tests/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'test/e2e/**/*.cy.{js,jsx,ts,tsx}'
   },
 })
